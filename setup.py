00cc0428ec0662c63174137a6c77b3fc72b1d6f8
Andrew Jewett <jewett.aij@gmail.com>
2019-12-06 06:37:10 -0800

    testing use of latex.codecogs.com in "long_description" string argument from setup.py.  hopefully this should make the equations more clear.

--- setup.py
+++ setup.py
@@ -13,29 +13,30 @@ setup(
 
 ##  Usage
 
-```
+```python
 def Superpose3D(X,    # <-- Nx3 array of coords for the "frozen" point cloud
                 x,    # <-- Nx3 array of coords for the "mobile" point cloud
-                w=None, #<- optional weights for the calculation of RMSD
-                        #     (default w_n = 1 for all n)
+                w = None, #<- optional weights for the calculation of RMSD
+                          #   (default w[n] = 1 for all n)
                 allow_rescale=False)  #<--attempt to rescale mobile point cloud?
 ```
 
 Superpose3D() takes two ordered lists (or numpy arrays) of xyz coordinates
-(*of the same length*, **N**) representing points in a point cloud
-(**X[n][i]** and **x[n][i]**). Treating them as rigid objects,
-"Superpose3D()" attempts to superimpose them using **rotations**,
-**translations**, and (optionally) **scale** transformations in order
-to minimize the root-mean-squared-distance (RMSD) between corresponding
-points from either point cloud, where RMSD is defined as:
-```
-   RMSD = sqrt( (Σ_n  w_n * Σ_i |X_ni - (Σ_j c*R_ij*x_nj + T_i)|^2) / (Σ_n w_n) )
-```
-If *w_n=None*, equal weights are used.  In that case:
-```
-   RMSD = sqrt( (Σ_n Σ_i |X_n - (Σ_j c*R_ij*x_j + T_i)|^2) / N )
-```
+(*of the same length*, **N**) representing points in a point cloud (**X** and
+**x**). Treating them as rigid objects, "Superpose3D()" attempts to superimpose
+them using **rotations**, **translations**, and (optionally) **scale**
+transformations in order to minimize the root-mean-squared-distance (RMSD)
+between corresponding points from either point cloud, where RMSD is defined as:
+
+<img src="http://latex.codecogs.com/gif.latex?\large&space;RMSD=\sqrt\left\sum_{n=1}^N\,w_n\,\sum_{i=1}^3 \left|X_{ni}-\left(\sum_{j=1}^3 c R_{ij}x_{nj}+T_i\right)\right|^2\quad\middle/\quad\sum_{n=1}^N w_n}\right}"/>
+
+If *w<sub>n</sub>* are omitted (ie. if *w<sub>n</sub> = None*),
+then equal weights are used.  In that case:
+
+<img src="http://latex.codecogs.com/gif.latex?\large&space;RMSD=\sqrt{\,\frac{1}{n}\,\sum_{n=1}^N\,\,\sum_{i=1}^3 \left|X_{ni}-\left(\sum_{j=1}^3 cR_{ij}x_{nj}+T_i\right)\right|^2}"/>
+
 ...where:
+
 ```
    T_j  = a translation vector (a 1-D numpy array containing x,y,z displacements),
    R_ij = a rotation matrix    (a 3x3 numpy array whose determinant = 1),
@@ -66,9 +67,9 @@ https://github.com/jewettaij/superpose3d_cpp
 
   url='https://github.com/jewettaij/superpose3d',
 
-  download_url='https://github.com/jewettaij/superpose3d/archive/v0.2.9.zip',
+  download_url='https://github.com/jewettaij/superpose3d/archive/v0.2.10.zip',
 
-  version='0.2.9',
+  version='0.2.10',
 
   install_requires=[
       'numpy',
