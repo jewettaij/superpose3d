d6853c51ff8309afb67671392e05397b2151dd19
Andrew Jewett <jewett.aij@gmail.com>
2022-03-20 23:54:25 -0700

    updated version info

--- setup.py
+++ setup.py
@@ -81,9 +81,9 @@ https://github.com/jewettaij/superpose3d_cpp
 
   url='https://github.com/jewettaij/superpose3d',
 
-  download_url='https://github.com/jewettaij/superpose3d/archive/v1.3.0.zip',
+  download_url='https://github.com/jewettaij/superpose3d/archive/v1.4.1.zip',
 
-  version='1.3.0',
+  version='1.4.1',
 
   install_requires=[
       'numpy',
